f742a9ad7804e388ca663fef9eab28c22f3df417
wnsk0414 <judyk344@gmail.com>
2023-03-28 14:35:12 +0900

    커밋2 메소드를 추가해서 코드를 수정함 빼기 기능을 완성함

--- clac.py
+++ clac.py
@@ -1,13 +1,19 @@
 ## 함수 선언부(=메소드)
+def add_func(n1, n2) :
+    retValue = n1 + n2
+    return retValue
 
-
-
+def sub_func(n1, n2) :
+    return n1 - n2
 
 ## 전역 변수부
-num1, num2, hap = 100, 200, 0
+num1, num2, result = 100, 200, 0
 
 
 
 ## 메인 코드부
-hap = num1 + num2
-print(num1,'+', num2, '=', hap)
+hap = add_func(num1, num2)
+print(num1, '+', num2, '=', hap)
+
+result = sub_func(num1, num2)
+print(num1, '-', num2, '=', result)
